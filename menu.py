e33c906f0a9adbe457591639d4313f7eb44608a1
pernention <tise90650@gmail.com>
2025-03-15 22:17:34 +0000

    Add files via upload

--- menu.py
+++ menu.py
@@ -1,64 +1,63 @@
 import pygame
 import sys
+import button
 
 pygame.init()
 
 
-# Creation of main variables, which can be referenced during all stages of the creation of the game
+# Variables
 HEIGHT = 800
 WIDTH = 600
 timer = pygame.time.Clock()
 fps = 60
 
-game_paused = False
+
 
 
 screen = pygame.display.set_mode((800,600))
-# creation of the screen window that the user will see when they open the game
+# Main Screen
 pygame.display.set_caption("Metamaze")
-# this changes the game name from pygame window to Metamaze, which is the name of the
-
-# creation of font, as well as text colour
-font = pygame.font.SysFont("arialblack", 25)
-text_color = (255, 0, 255)
 
 
+class Menu:
+    ...
+class Game:
+    ...
 
-def draw_buttons(text, font, text_color, x, y)
-run = True
+
+    
+game_state = "Main Menu"
+menu = Menu()
+game = Game()
+
+# Allows Exit game
 while True:
+    timer.tick(fps)
+    screen.fill("purple")
+
+    if game_state == "main menu":
+        state = menu
+    elif game_state == "game":
+        state = game  
 
 
-    
-    class Menu:
-    
-        def __init__(self, ):
-            self.stuff = passed_stuff_however_necessary
-        
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit
+            exit()
+            # While true loop is closed
+        else:
+            if state.event(event) is None:
+                exit()
 
-        def event(self, event):
-            # checks for events
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-                return None  
-            # when the event key is pressed, nothing happens
-            elif event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-                game_paused = True
-               
+    state.update()
+    state.draw(screen)
 
-                # when the spacebar key is pressed, the game is paused
+
+    pygame.display.update()
 
 
 
-            while True:
-                timer.tick(fps)
-                screen.fill("purple")
-                for event in pygame.event.get():
-                    if event.type == pygame.QUIT:
-                        pygame.quit()
-                        exit()
-                    else:
-                        if state.event(event) is None:
-                            exit()
 
 
 
